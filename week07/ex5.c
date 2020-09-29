82d8584d32f72369f8fd182130ef7c53de5c6b02
Amirov Aldiyar <amirovaldiar@gmail.com>
2020-09-29 23:13:06 +0300

    week 07

--- week07/ex5.c
+++ week07/ex5.c
@@ -3,7 +3,7 @@ int main() {
   char **s=malloc(1);
   char foo[] = "Hello World";
   *s = foo;
-  printf("s is %s\n", *s);
+  printf("s is %p\n", s);
   s[0] = foo;
   printf("s[0] is %s\n",s[0]);
   return(0);
